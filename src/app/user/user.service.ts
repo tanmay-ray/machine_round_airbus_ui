8eb8aec5e4c2d571ad7e680f1c261df99d78b368
Tanmay_Ray <tanmay_ray@dellteam.com>
2021-04-19 02:05:30 +0530

    using env to determine BE host url for different envs

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -1,19 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
 import { User } from '../model/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private baseUrl = environment.baseUrl;
 
   constructor(private httpClient: HttpClient) { }
 
   getUser(email: string) {
-    return this.httpClient.get<User>(`http://localhost:8080/user/${email}`);
+    return this.httpClient.get<User>(`${this.baseUrl}/user/${email}`);
   }
 
   getUserList() {
-    return this.httpClient.get<User[]>(`http://localhost:8080/users`);
+    return this.httpClient.get<User[]>(`${this.baseUrl}/users`);
   }
 }
